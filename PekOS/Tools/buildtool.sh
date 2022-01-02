d182a22116b7c91f1e25a821dc8b20fa3ab9e873
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2022-01-02 14:19:35 +0100

    Update buildtool.sh

--- PekOS/Tools/buildtool.sh
+++ PekOS/Tools/buildtool.sh
@@ -1,7 +1,7 @@
 
-sudo apt-get install nasm && continue || echo FAILED to install nasm
-sudo apt-get install qemu-system-x86 && continue || echo FAILED to install Qemu
-sudo apt-get install git && echo git insatlled successfully || echo FAILED to install git
+sudo apt-get install nasm && continue || echo NASM INSTALLATION FAILED
+sudo apt-get install qemu-system-x86 && continue || echo QEMU INSTALLATION FAILED
+sudo apt-get install git && echo git insatlled successfully || echo GIT INSTALLATION FAILED
 
 git clone https://github.com/StjepanBM1/PekOS
 
